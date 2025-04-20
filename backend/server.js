c2d8df89e93d421fb2ea0127b77423dfdaed11ca
Abdelwahad Dahmani <33489898+flixsoon@users.noreply.github.com>
2025-04-20 18:04:40 +0100

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // Connexion à MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/book-management', {
+mongoose.connect('mongodb://localhost:27017/book-management', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4 // Use IPv4, skip trying IPv6
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB:', err));
